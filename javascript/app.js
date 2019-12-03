e881fcbc4c59f71ce82be2eaf7c20fdf010c7418
Ayesha Munawar <54625611+AyeshaMunawar-profile@users.noreply.github.com>
2019-12-03 18:10:24 +0500

    budget-app-1> Add event listmner to the ENTER and add item button in the home page

--- javascript/app.js
+++ javascript/app.js
@@ -4,15 +4,34 @@
 // After execution of each outer function the closures are created to which only the
 // interfaces have access thus creating data encapsulation for the inner functionality and private data
 // variables of each controller
+//BUDGET CONTROLLER
 let budgetController = (function(){
 
 })();
-
+//UI CONTROLLER
 let UIController = (function(){
 
 })();
-
-// connect Ui controller and budget controller
+//GLOBAL APP CONTROLLER
+// Connect Ui controller and budget controller
 let controller = (function(budgetCrl, UICrl){
+    var crlAddItem= function(){
+        // 1. get the field input data
+
+        // 2. add the item to the list as income or expense through budget controller
+
+        // 3. add the item to the UI list of expenses or income items based on the type of item
+
+        // 4. calculate the overall budget
+
+        // 5. display the overall budget on UI
+        alert("it works");
+    };
+    document.querySelector(".add__btn").addEventListener('click', crlAddItem);
+    document.addEventListener('keypress', function(event){
+        if(event.key==='Enter' || event.keyCode===13 || event.which ===13) {
+            crlAddItem();
+        }
+    });
 
 })(budgetController,UIController);
