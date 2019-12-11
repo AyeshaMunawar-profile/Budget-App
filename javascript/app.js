025a87a9cf049a9ed8843d7cd1d48174adfbb22d
Ayesha Munawar <54625611+AyeshaMunawar-profile@users.noreply.github.com>
2019-12-11 12:11:17 +0500

    budget-app-1> 1)Add new expense or income to the appropriate data structure 2) Create datastrucute to store count , expense , income , percentages and list of individuaol expense and income 3) reate function constructors for expense and income 4) calculate the ID for each item expense or incoem when added to the respective list 5) creat einitialization function in controller to make code cleaner

--- javascript/app.js
+++ javascript/app.js
@@ -6,8 +6,63 @@
 // variables of each controller
 //BUDGET CONTROLLER
 let budgetController = (function () {
+        //function constructor for income object
+        let Income = function (id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+        //function constructor of expense object
+        let Expense = function (id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+        let budgetData = {
+            allItems: {
+                exp: [],
+                inc: []
+            },
+            totalItems: {
+                totalIncome: 0,
+                totalExpenses: 0
+            },
+            percentages: {
+                percentageIncome: 0,
+                percentageExpense: 0
+            },
+            overallBudget: 0,
+            itemsCount: 0
+        };
+        return {
+            addItems: function (type, des, val) {
+                let newItem, ID;
+                // create new ID
+                if (budgetData.allItems[type].length === 0) {
+                    ID = 0;
+                } else {
+                    ID = budgetData.allItems[type][budgetData.allItems[type].length - 1].id + 1;
+                }
+                budgetData.itemsCount++;
+                // create new item based on type i.e inc or exp
+                if (type === 'exp') {
+                    newItem = new Expense(ID, des, val);
 
-})();
+                } else if (type === 'inc') {
+                    newItem = new Income(ID, des, val);
+                }
+                // add new item to the respective data structure based on type i.e exp or inc
+                budgetData.allItems[type].push(newItem);
+                return newItem;
+            }
+            ,
+            testing: function () {
+                console.log(budgetData);
+            }
+        }
+
+    }
+)();
 //UI CONTROLLER
 let UIController = (function () {
     var DOMstring = {
@@ -24,7 +79,7 @@ let UIController = (function () {
                 value: document.querySelector(DOMstring.addValue).value
             };
         },
-        getDOMStrings: function(){
+        getDOMStrings: function () {
             return DOMstring;
         }
     };
@@ -32,26 +87,37 @@ let UIController = (function () {
 //GLOBAL APP CONTROLLER
 // Connect Ui controller and budget controller
 let controller = (function (budgetCrl, UICrl) {
-    let DOMstrings= UICrl.getDOMStrings();
+    let setupEventListeners = function () {
+        let DOMstrings = UICrl.getDOMStrings();
+        document.querySelector(DOMstrings.addButton).addEventListener('click', crlAddItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.key === 'Enter' || event.keyCode === 13 || event.which === 13) {
+                crlAddItem();
+            }
+        });
+    };
     let crlAddItem = function () {
+        let input, newItem;
         // 1. get the field input data
-        let input = UICrl.getInput();
-        console.log(input);
-        // 2. add the item to the list as income or expense through budget controller
-
+        input = UICrl.getInput();
+        // 2. add the item to the appropriate data structure as income or expense through budget controller
+        newItem = budgetCrl.addItems(input.type, input.description, input.value);
+        budgetCrl.testing();
         // 3. add the item to the UI list of expenses or income items based on the type of item
 
         // 4. calculate the overall budget
 
+
         // 5. display the overall budget on UI
     };
 
-
-    document.querySelector(DOMstrings.addButton).addEventListener('click', crlAddItem);
-    document.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter' || event.keyCode === 13 || event.which === 13) {
-            crlAddItem();
+    return {
+        init: function () {
+            console.log("The Application has started");
+            setupEventListeners();
         }
-    });
+    }
 
 })(budgetController, UIController);
+
+controller.init();
